aae68908b99c30959952ac0234de95b4f0ed6196
HETLimbachiya <139550333+hetlimbachiya0810@users.noreply.github.com>
2025-07-26 03:41:34 +0530

    Sprint 2 Early Test

--- backend/init_db.py
+++ backend/init_db.py
@@ -1,11 +1,18 @@
 import asyncio
+import logging
 from db.database import init_db
 
+
+
 async def main():
-    """Initialize the database tables asynchronously"""
-    print("Creating database tables...")
-    await init_db()
-    print("Tables created successfully!")
+    try:
+        """Initialize the database tables asynchronously"""
+        print("Creating database tables...")
+        await init_db()
+        print("Tables created successfully!")
+    except Exception as e:
+        logging.error(f"Error creating tables: {str(e)}")
+        raise
 
 if __name__ == "__main__":
     asyncio.run(main())
